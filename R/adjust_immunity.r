0cd7974a03015e96577ef9fdac103cd2e2cabceb
sbfnk <sebastian.funk@lshtm.ac.uk>
2018-09-19 13:49:39 +0100

    adjust_immunity syntax update

--- R/adjust_immunity.r
+++ R/adjust_immunity.r
@@ -1,9 +1,9 @@
 ##' Estimate reproduction number from contact information and immunity profile
 ##'
 ##' This takes a contact survey to derive a contact matrix and rescales contacts to represent contacts with susceptibles. This is then combined with information on the basic reproduction number R0 to calculate the effective or net reproduction number.
-##' @param immunity immunity profiles; this should be given as a vector of the same length as \code{age.limits} or a data.frame; if given as a vector, each element of the vector should contain a value <1 representing the proportion of the population immune in the corresponding age group; any element set to "herd" will be set to 1-1/R0; if given as a data.frame, the number of rows need to be \code{n}, and every row will correspond to one of the samples
-##' @param model "age-specific" (default) or "homogeneous"; if set to "homogeneous", the contact survey is not used to generate a contact matrix, but only for demographic information; beyond demography, random mixing is assumed.
-##' @param ... any further parameters for \code{\link{contact_matrix}}
+##' @param mixing A mixing matrix or set of mixing matrices, as returned by \code{\link{contact_matrix}}
+##' @param demography A data frame with a \code{population} column, indicating the populations of the age groups given in \code{immunity}
+##' @param immunity immunity profiles; this should be given as a vector of the same length as the number of rows/columns of the mixing matrix (or matrices); if given as a vector, each element of the vector should contain a value <1 representing the proportion of the population immune in the corresponding age group; any element set to "herd" will be set to 1-1/R0; if given as a data.frame, the number of rows need to be \code{n}, and every row will correspond to one of the samples
 ##' @return a list contain vectors of adjusted immunities
 ##' @importFrom socialmixr contact_matrix
 ##' @author Sebastian Funk
@@ -14,7 +14,7 @@ adjust_immunity <- function(mixing, demography, immunity)
 
     if (!("matrices" %in% names(mixing)))
     {
-        mixing$matrices <- list(mixing)
+        mixing <- list(matrices=list(mixing))
     }
     if (is.vector(immunity))
     {
@@ -25,7 +25,7 @@ adjust_immunity <- function(mixing, demography, immunity)
     {
         ## rescale to a_{ij} n_j delta_i
         mixing_normalised <-
-            t(t(mixing$matrices[[x]]$matrix * demography$population) /
+            t(t(mixing$matrices[[x]] * demography$population) /
               demography$population)
         ## rescale by immunity
         mixing_immunised <- mixing_normalised * (1 - immunity[x, ])
@@ -34,5 +34,5 @@ adjust_immunity <- function(mixing, demography, immunity)
                Re(eigen(mixing_normalised)$values[1]))
     })
 
-    return(unlist(ret))
+    return(ret)
 }
