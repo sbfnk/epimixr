a97edcec13575cd4e26758e663df8955aa825c7c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-03 21:24:13 +0000

    don't require demography

--- R/adjust_immunity.r
+++ R/adjust_immunity.r
@@ -2,7 +2,7 @@
 ##'
 ##' This takes a contact survey to derive a contact matrix and rescales contacts to represent contacts with susceptibles. This is then combined with information on the basic reproduction number R0 to calculate the effective or net reproduction number.
 ##' @param mixing A mixing matrix or set of mixing matrices, as returned by \code{\link{contact_matrix}}
-##' @param demography A data frame with a \code{population} column, indicating the populations of the age groups given in \code{immunity}
+##' @param demography A data frame with a \code{population} column, indicating the populations of the age groups given in \code{immunity}; if it is not given, it will be assumed that the mixing matrix is already normalised
 ##' @param immunity immunity profiles; this should be given as a vector of the same length as the number of rows/columns of the mixing matrix (or matrices); if given as a vector, each element of the vector should contain a value <1 representing the proportion of the population immune in the corresponding age group; any element set to "herd" will be set to 1-1/R0; if given as a data.frame, the number of rows need to be \code{n}, and every row will correspond to one of the samples
 ##' @return a list contain vectors of adjusted immunities
 ##' @importFrom socialmixr contact_matrix
@@ -21,12 +21,17 @@ adjust_immunity <- function(mixing, demography, immunity)
         immunity <- matrix(rep(immunity, length(mixing$matrices)), byrow=TRUE,
                            nrow=length(mixing$matrices))
     }
+    demography_given <- !missing(demography)
     ret <- lapply(seq_along(mixing$matrices), function(x)
     {
-        ## rescale to a_{ij} n_j delta_i
-        mixing_normalised <-
-            t(t(mixing$matrices[[x]] * demography$population) /
-              demography$population)
+        if (demography_given) {
+            ## rescale to a_{ij} n_j delta_i
+            mixing_normalised <-
+                t(t(mixing$matrices[[x]] * demography$population) /
+                  demography$population)
+        } else {
+            mixing_normalised <- mixing$matrices[[x]]
+        }
         ## rescale by immunity
         mixing_immunised <- mixing_normalised * (1 - immunity[x, ])
         ## calculate adjImm = 1 -
