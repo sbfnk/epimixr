2b6418d8a97df145332e6b35a10fcd481f7e3c34
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-05-29 15:21:31 +0100

    adjust_immunity: update documentation

--- R/adjust_immunity.r
+++ R/adjust_immunity.r
@@ -2,7 +2,7 @@
 ##'
 ##' This takes a contact survey to derive a contact matrix and rescales contacts to represent contacts with susceptibles. This is then combined with information on the basic reproduction number R0 to calculate the effective or net reproduction number.
 ##' @param mixing A mixing matrix or set of mixing matrices, as returned by \code{\link{contact_matrix}}
-##' @param demography A data frame with a \code{population} column, indicating the populations of the age groups given in \code{immunity}; if it is not given, it will be assumed that the mixing matrix is already normalised
+##' @param demography A vector of populations (absolute or relative) of the age groups given in \code{immunity}; if it is not given, it will be assumed that the mixing matrix is already normalised
 ##' @param immunity immunity profiles; this should be given as a vector of the same length as the number of rows/columns of the mixing matrix (or matrices); if given as a vector, each element of the vector should contain a value <1 representing the proportion of the population immune in the corresponding age group; any element set to "herd" will be set to 1-1/R0; if given as a data.frame, the number of rows need to be \code{n}, and every row will correspond to one of the samples
 ##' @return a list contain vectors of adjusted immunities
 ##' @importFrom socialmixr contact_matrix
