57319952f744103328a40fa980ed767f0d7de301
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-06-03 20:43:36 +0100

    project immunity: finish first version

--- R/project_immunity.r
+++ R/project_immunity.r
@@ -1,4 +1,4 @@
-project_immunity <- function(baseline.immunity, coverage, schedule)
+project_immunity <- function(baseline.immunity, coverage, schedule, maternal.immunity, efficacy)
 {
   ## checks
   if (missing(baseline.immunity)) stop("'coverage' must be provided")
@@ -9,20 +9,47 @@ project_immunity <- function(baseline.immunity, coverage, schedule)
   }
 
   ## convert baseline to annual immunity
-  bdf <- data.frame(lower.age.limit=as.integer(names(baseline.immunity)),
+  lower_age_limits <- as.integer(names(baseline.immunity))
+  bdf <- data.frame(lower.age.limit=lower_age_limits,
                     immunity=baseline.immunity)
   min_age <- min(bdf$lower.age.limit)
   max_age <- max(bdf$lower.age.limit)
   df <- data.frame(lower.age.limit=
-                     do.call(seq, as.list(range(bdf$lower.age.limit))))
+                     do.call(seq, as.list(range(lower_age_limits))))
   df[df$lower.age.limit %in% bdf$lower.age.limit, "immunity"] <- bdf$immunity
   ## fill
   df$immunity <- c(NA, na.omit(df$immunity))[cumsum(!is.na(df$immunity))+1]
 
-  for (i in seq_len(length(coverage[[1]]))) {
-    ## move all one age group up
-    df[seq(2, nrow(df) - 1), "immunity"] <- df[seq(1, nrow(df) - 2), "immunity"]
-    ## implement vaccination schedule
-    df[df$lower.age.limit==1, "immunity"] <- 0
+  scaling_factor <- min(df[df$lower.age.limit==1, "immunity"] / coverage[[1]][1], 1)
+  oldest <- df[nrow(df), ]
+
+  if (length(coverage[[1]]) > 1) {
+    for (i in seq(2, length(coverage[[1]]))) {
+      ## move all one age group up
+      df$lower.age.limit <- df$lower.age.limit + 1
+      ## implement vaccination schedule
+      df <- df[df$lower.age.limit > 2, ]
+      first_two_years <-
+        data.frame(lower.age.limit=c(0, 1, 2),
+                   immunity=c(maternal.immunity,
+                              coverage[[1]][i-1] * scaling_factor * efficacy,
+                              coverage[[1]][i] * efficacy))
+      df <- rbind(first_two_years, df)
+    }
+    df <- df[df$lower.age.limit < oldest$lower.age.limit, ]
+    df <- rbind(df, oldest)
+    rownames(df) <- seq_len(nrow(df))
   }
+
+  ## aggregate by age groups
+  df$lower.age.limit <-
+    socialmixr::reduce_agegroups(df$lower.age.limit, union(0, lower_age_limits))
+
+  summarised <- by(df, list(df$lower.age.limit), function(x)
+    c(
+      lower.age.limit=unique(x$lower.age.limit),
+      immunity=mean(x$immunity)
+    ))
+  df <- do.call(rbind, summarised)
+  return(df)
 }
